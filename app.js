55007409089db51e522e058da20bcf306263b7a3
EnterOrion <orionwesson@gmail.com>
2022-10-30 19:19:00 -0300

    Create user input function

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-let options = ["Rock", "Paper", "Scissors"];
+let options = ["rock", "paper", "scissors"];
+
+
+function getUserChoice() {
+    let userChoice = document.getElementById("userChoice").value;
+    console.log(userChoice);
+    userChoice = userChoice.toLowerCase();
+    //if (userChoice != "rock" || userChoice != "paper" || userChoice != "scissors") {
+    //    alert("Please enter a valid choice!");
+    //}
+    //else {
+    //     return userChoice
+    //}
+}
 
 function getComputerChoice() {
     return options[Math.floor(Math.random() * options.length)];
 }
 
-alert(getComputerChoice());
+alert(getUserChoice());
